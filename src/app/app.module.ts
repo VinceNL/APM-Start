3516fa1198fc848c3984c8ea959a1f9970054b65
vvancampen <vvancampen@msn.com>
2021-10-21 00:18:10 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { convertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    ProductListComponent,
+    convertToSpacesPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
