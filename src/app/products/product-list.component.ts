97e75c0aeee04bda5f318ce619e22ce02e3cd1a0
vvancampen <vvancampen@msn.com>
2021-10-24 22:58:30 +0200

    Chapter 9 done

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -7,6 +8,8 @@ import { IProduct } from "./product";
     styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+    constructor(private productService: ProductService) {}
+
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -22,28 +25,7 @@ export class ProductListComponent implements OnInit {
     }
     
     filteredProducts: IProduct[] = [];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
-    ]; 
+    products: IProduct[] = []; 
 
     performFilter(filterBy: string): IProduct[] {
       filterBy.toLocaleLowerCase();
@@ -56,6 +38,12 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit() {
-      this.listFilter = 'cart';
-    };
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
+      //this.listFilter = 'cart';
+    }
+
+    onRatingClicked(message: string): void {
+      this.pageTitle = `Product List: ${message}`;
+    }
 }
